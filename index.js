b0f3b09d9f594188b24be9f44a15dbc203118f4e
Shivanshu Gupta <shivanshukgupta@gmail.com>
2023-12-26 21:41:31 +0530

    done

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3002;
 
-const paymentServer = "https://ibrpay.com/api/PayoutLive.aspx"
+const paymentOutServer = "https://ibrpay.com/api/PayoutLive.aspx"
 const APIID = "API1022"
-const Token = ""
+const Token = "a5a8fe37-d315-41f2-b2c7-4a99e8ea3597"
 
 function print(...msg) {
     console.log(...msg);
@@ -23,7 +23,7 @@ app.get('/', async (req, res) => {
 
 // Endpoint for payout transfer
 app.post('/payout-transfer', async (req, res) => {
-    print(`post request at /payment-transfer: `);
+    print(`post request at /payout-transfer: `);
     print(`req.body = `, req.body);
     const err = validatePayoutTransferRequest(req.body);
     if (err != null) {
@@ -35,7 +35,7 @@ app.post('/payout-transfer', async (req, res) => {
     req.body.Token = Token;
     req.body.MethodName = "payout";
     try {
-        const response = await axios.post(paymentServer, req.body);
+        const response = await axios.post(paymentOutServer, req.body);
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: `${error}` });
@@ -71,7 +71,7 @@ app.post('/payout-status', async (req, res) => {
     req.body.Token = Token;
     req.body.MethodName = "checkstatus";
     try {
-        const response = await axios.post(paymentServer, req.body);
+        const response = await axios.post(paymentOutServer, req.body);
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: `${error}` });
@@ -79,6 +79,122 @@ app.post('/payout-status', async (req, res) => {
     }
 });
 
+// Wallet Load --------------------------------
+
+const walletLoadServer = "https://ibrpay.com/api/GetAmount.aspx";
+
+// Endpoint for payout transfer
+app.post('/payment-transfer', async (req, res) => {
+    print(`post request at /payment-transfer: `);
+    print(`req.body = `, req.body);
+    var err = req.body.amount ? null : "amount is required";
+    err = err || req.body.redirect_url ? null : "redirect_url is required";
+    if (err != null) {
+        console.error(err);
+        res.status(400).json({ error: err });
+        return;
+    }
+    req.body.APIID = APIID;
+    req.body.Token = Token;
+    req.body.MethodName = "collectionrequest";
+    try {
+        const response = await axios.post(walletLoadServer, req.body);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: `${error}` });
+        print("Error: ", error);
+    }
+});
+
+app.post('/payment-callback', async (req, res) => {
+    print(`post request at /callback: `);
+    print(`req.body = `, req.body);
+    try {
+        // TODO: handle the callback here
+        res.status(200).json({
+            status: 'success',
+            message: 'Payment Callback handled successfully',
+        });
+    } catch (error) {
+        res.status(500).json({ error: `${error}` });
+        print("Error: ", error);
+    }
+});
+
+app.post('/payment-status', async (req, res) => {
+    print(`post request at /payment-status: `);
+    print(`req.body = `, req.body);
+    const err = !req.body.amount ? "amount is required" : null;
+    err = err || !req.body.OrderKeyId ? "OrderKeyId is required" : null;
+    if (err != null) {
+        console.error(err);
+        res.status(400).json({ error: err });
+        return;
+    }
+    req.body.APIID = APIID;
+    req.body.Token = Token;
+    req.body.MethodName = "CheckStatus";
+    try {
+        const response = await axios.post(walletLoadServer, req.body);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: `${error}` });
+        print("Error: ", error);
+    }
+});
+
+// Wallet Load --------------------------------
+
+const upiTransferServer = "https://ibrpay.com/api/UPITransferLive.aspx";
+
+// Endpoint for UPI Transfer
+app.post('/upi-transfer', async (req, res) => {
+    print(`post request at /upi-transfer: `);
+    print(`req.body = `, req.body);
+    var err = req.body.OrderID ? null : "OrderID is required";
+    err = err || req.body.Name ? null : "Name is required";
+    err = err || req.body.Amount ? null : "Amount is required";
+    err = err || req.body.vpa ? null : "vpa is required";
+    if (err != null) {
+        console.error(err);
+        res.status(400).json({ error: err });
+        return;
+    }
+    req.body.APIID = APIID;
+    req.body.Token = Token;
+    req.body.MethodName = "upitransfer";
+    try {
+        const response = await axios.post(upiTransferServer, req.body);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: `${error}` });
+        print("Error: ", error);
+    }
+});
+
+app.post('/upi-verification', async (req, res) => {
+    print(`post request at /upi-verification: `);
+    print(`req.body = `, req.body);
+    const err = !req.body.OrderID ? "OrderID is required" : null;
+    err = err || !req.body.vpa ? "vpa is required" : null;
+    if (err != null) {
+        console.error(err);
+        res.status(400).json({ error: err });
+        return;
+    }
+    req.body.APIID = APIID;
+    req.body.Token = Token;
+    req.body.MethodName = "verification";
+    try {
+        const response = await axios.post(upiTransferServer, req.body);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: `${error}` });
+        print("Error: ", error);
+    }
+});
+
+// ========================================================
 app.listen(port, () => {
     print(`Server is running on port ${port}`);
 });
